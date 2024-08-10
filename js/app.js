7dbc9dafca268bc743caf22fefa5d6a73cdd2b85
igorp332 <igorpontes8@gmail.com>
2024-08-10 09:31:04 -0300

    realizando o sorteio

--- js/app.js
+++ js/app.js
@@ -1,16 +1,40 @@
-let amigos = []
+let amigos = [];
 
 function adicionar() {
   let amigo = document.getElementById("nome-amigo");
   let lista = document.getElementById("lista-amigos");
+  amigos.push(amigo.value);
   if (lista.textContent == "") {
     lista.textContent = amigo.value;
   } else {
     lista.textContent = lista.textContent + ", " + amigo.value;
   }
-  amigo.value = ""
+  amigo.value = "";
 }
 
-function sortear () {
+function sortear() {
+  embaralha(amigos);
+  let sorteio = document.getElementById("lista-sorteio");
 
+  for (let i = 0; i < amigos.length; i++) {
+    if (i == amigos.length - 1) {
+      sorteio.innerHTML =
+        sorteio.innerHTML + amigos[i] + " --> " + amigos[0] + "<br>";
+    } else {
+      sorteio.innerHTML =
+        sorteio.innerHTML + amigos[i] + " --> " + amigos[i + 1] + "<br>";
+    }
+  }
+}
+
+function embaralha(lista) {
+  let indice = lista.length;
+
+  while (indice) {
+    const indiceAleatorio = Math.floor(Math.random() * indice--);
+    [lista[indice], lista[indiceAleatorio]] = [
+      lista[indiceAleatorio],
+      lista[indice],
+    ];
+  }
 }
